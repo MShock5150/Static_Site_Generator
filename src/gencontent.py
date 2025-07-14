bd22eaacec6a491a33e00218bdfce9bf96100189
MShock5150 <corleym5150@gmail.com>
2025-07-14 15:55:12 -0500

    build: generate site for deployment

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
     raise Exception("Error: All pages need a single h1 header")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         markdown = file.read()
@@ -18,7 +18,12 @@ def generate_page(from_path, template_path, dest_path):
         template = file.read()
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    final_html = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
     dest_dir_path = os.path.dirname(dest_path)
     os.makedirs(dest_dir_path, exist_ok=True)
     with open(dest_path, "w") as file:
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 from gencontent import generate_page
@@ -18,33 +19,39 @@ def copy_directory_recursive(source_path, dest_path):
             copy_directory_recursive(source_file_path, dest_file_path)
 
 
-def generate_page_recursive(source_path, template_path, dest_path):
+def generate_page_recursive(source_path, template_path, dest_path, basepath):
     files = os.listdir(source_path)
     for file in files:
         source_file_path = os.path.join(source_path, file)
         dest_file_path = os.path.join(dest_path, file)
         if os.path.isfile(source_file_path) and source_file_path.endswith(".md"):
             html_dest_path = dest_file_path.replace(".md", ".html")
-            generate_page(source_file_path, template_path, html_dest_path)
+            generate_page(source_file_path, template_path, html_dest_path, basepath)
         elif os.path.isdir(source_file_path):
             os.makedirs(dest_file_path, exist_ok=True)
-            generate_page_recursive(source_file_path, template_path, dest_file_path)
+            generate_page_recursive(
+                source_file_path, template_path, dest_file_path, basepath
+            )
 
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     current_dir = os.getcwd()
     static_source = os.path.join(current_dir, "static")
-    public_dest = os.path.join(current_dir, "public")
-    print(f"Cleaning public directory: {public_dest}")
-    if os.path.exists(public_dest):
-        shutil.rmtree(public_dest)
-    os.mkdir(public_dest)
-    print(f"Copying from {static_source} to {public_dest}")
-    copy_directory_recursive(static_source, public_dest)
+    dest_dest = os.path.join(current_dir, "docs")
+    print(f"Cleaning destination directory: {dest_dest}")
+    if os.path.exists(dest_dest):
+        shutil.rmtree(dest_dest)
+    os.mkdir(dest_dest)
+    print(f"Copying from {static_source} to {dest_dest}")
+    copy_directory_recursive(static_source, dest_dest)
     content_source = os.path.join(current_dir, "content")
     template_path = os.path.join(current_dir, "template.html")
-    print(f"Generating pages from {content_source} to {public_dest}")
-    generate_page_recursive(content_source, template_path, public_dest)
+    print(f"Generating pages from {content_source} to {dest_dest}")
+    generate_page_recursive(content_source, template_path, dest_dest, basepath)
 
 
 main()
